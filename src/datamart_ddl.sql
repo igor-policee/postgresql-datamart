b7dcbb7f0c73edf0f9b79ee120ab1ca2c6eff8b2
Igor Polishchuk <igor.policee@gmail.com>
2022-06-04 20:33:51 +0300

    version 5

--- src/datamart_ddl.sql
+++ src/datamart_ddl.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS de.analysis.dm_rfm_segments;
 -- Создание витрины
 CREATE TABLE IF NOT EXISTS de.analysis.dm_rfm_segments (
     "user_id" int NOT NULL PRIMARY KEY,
-    "recency" smallint NOT NULL CHECK("recency" IN (1, 2, 3, 4, 5)),
-    "frequency" smallint NOT NULL CHECK("frequency" IN (1, 2, 3, 4, 5)),
-    "monetary_value" smallint NOT NULL CHECK("monetary_value" IN (1, 2, 3, 4, 5))
+    "recency" smallint NOT NULL CHECK(1 <= "recency" AND "recency" <= 5),
+    "frequency" smallint NOT NULL CHECK(1 <= "frequency" AND "frequency" <= 5),
+    "monetary_value" smallint NOT NULL CHECK(1 <= "monetary_value" AND "monetary_value" <= 5)
 );
